bd8381d82e0357ad19888e17bd9e747a8d52fd2e
Carlos Pradenas <carlpradenas@gmail.com>
2020-05-17 23:22:33 -0400

    add customers crud

---
+++ functions/src/controllers/customers.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import { db } from '../index';
+
+export const all = async (req: Request, res: Response) => {
+    const newsQuerySnapshot = await db.collection('customers').get();
+    const newsletters: any[] = [];
+    newsQuerySnapshot.forEach(doc => {
+        newsletters.push({
+            id: doc.id,
+            data: doc.data(),
+            createdAt: doc.createTime,
+            updatedAt: doc.updateTime
+        });
+    });
+    return res.status(200).json(newsletters);
+}
+
+export const one = async (req: Request, res: Response) => {
+    try {
+        const newOne = await db.collection('customers').doc(req.params.id).get();
+        res.status(200).json({id: newOne.id, data: newOne.data(), createdAt: newOne.createTime, updatedAt: newOne.updateTime});
+    } catch(error) {
+        res.status(500).send(error);
+    }
+}
+
+export const add = async (req: Request, res: Response) => {
+    try {
+        const newDoc = await db.collection('customers').add(req.body);
+        res.status(201).json({ status: 'created', id: newDoc.id });
+    } catch(error) {
+        res.status(500).send(error);
+    }
+}
+
+export const update = async (req: Request, res: Response) => {
+    try {
+        await db.collection('customers').doc(req.params.id).set(req.body, { merge: true })
+        res.status(201).json({ status: 'updated', id: req.params.id });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+export const remove = async (req: Request, res: Response) => {
+    try {
+        await db.collection('customers').doc(req.params.id).delete();
+        res.status(201).json({ status: 'deleted', id: req.params.id });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
--- functions/src/routes/index.ts
+++ functions/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import health from './health';
 import news from './news.routes';
+import customers from './customers.routes';
 
 const router = Router();
 const prefix = 'v1';
 router.use('/', health);
-router.use(`/${prefix}/news/`, news);
+router.use(`/${prefix}/news`, news);
+router.use(`/${prefix}/customers`, customers);
 
 export default router;
